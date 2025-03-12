d190e1ecb78f44097a9d58db32c20fa4c963abca
jurio108 <jurio108@gmail.com>
2025-03-12 15:45:37 +0900

    프로젝트 세팅

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navigation />
+      <h1>NotFound</h1>
+    </div>
+  );
+}
